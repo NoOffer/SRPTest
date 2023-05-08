8f6d403342e64d0dd84995d56040d82aa2b7f90b
Nofer <sxx261@case.edu>
2023-05-08 15:43:21 -0400

    Added SRP Unlit Rendering

--- Assets/SRP Scripts/TestRPInstance.cs
+++ Assets/SRP Scripts/TestRPInstance.cs
@@ -1,10 +1,18 @@
 using UnityEngine;
 using UnityEngine.Rendering;
+using Conditional = System.Diagnostics.ConditionalAttribute;
 
 public class TestPipelineInstance : RenderPipeline
 {
+    private Material errorMaterial;
+
     public TestPipelineInstance()
     {
+        Shader errorShader = Shader.Find("Hidden/InternalErrorShader");
+        errorMaterial = new Material(errorShader)
+        {
+            hideFlags = HideFlags.HideAndDontSave
+        };
     }
 
     protected override void Render(ScriptableRenderContext renderContext, Camera[] cameras)
@@ -13,12 +21,6 @@ public class TestPipelineInstance : RenderPipeline
         {
             Render(renderContext, camera);
         }
-
-        // Draw skybox
-        renderContext.DrawSkybox(cameras[0]);
-
-        // Submit buffered outputs
-        renderContext.Submit();
     }
 
     private void Render(ScriptableRenderContext renderContext, Camera camera)
@@ -26,8 +28,11 @@ public class TestPipelineInstance : RenderPipeline
         // Apply camera settings to correctly setup the context
         renderContext.SetupCameraProperties(camera);
 
-        // Create a command buffer
-        CommandBuffer buffer = new CommandBuffer();
+        // Create command buffer
+        CommandBuffer buffer = new CommandBuffer
+        {
+            name = "Command Buffer"
+        };
 
         // Setup commands
         buffer.ClearRenderTarget(
@@ -39,5 +44,52 @@ public class TestPipelineInstance : RenderPipeline
         // Execute and release the command buffer
         renderContext.ExecuteCommandBuffer(buffer);
         buffer.Release();
+
+        // Fix scene view UI
+#if UNITY_EDITOR
+        if (camera.cameraType == CameraType.SceneView)
+        {
+            ScriptableRenderContext.EmitWorldGeometryForSceneView(camera);
+        }
+#endif
+
+        // Culling process
+        ScriptableCullingParameters cullingParameters;
+        if (!camera.TryGetCullingParameters(out cullingParameters))
+        {
+            return;
+        }
+        CullingResults cullingResults = renderContext.Cull(ref cullingParameters);
+
+        // Drawing phase
+        // Setup render settings
+        DrawingSettings drawSettings = new DrawingSettings(new ShaderTagId("ForwardBase"), new SortingSettings());
+
+        FilteringSettings filterSettings = FilteringSettings.defaultValue;
+
+        // Draw error materials
+        drawSettings.overrideMaterial = errorMaterial;
+        renderContext.DrawRenderers(
+            cullingResults, ref drawSettings, ref filterSettings
+        );
+
+        drawSettings = new DrawingSettings(new ShaderTagId("SRPDefaultUnlit"), new SortingSettings());
+        // Draw Opaque
+        filterSettings.renderQueueRange = RenderQueueRange.opaque;
+        renderContext.DrawRenderers(
+            cullingResults, ref drawSettings, ref filterSettings
+        );
+
+        // Draw skybox
+        renderContext.DrawSkybox(camera);
+
+        // Draw transparent
+        filterSettings.renderQueueRange = RenderQueueRange.transparent;
+        renderContext.DrawRenderers(
+            cullingResults, ref drawSettings, ref filterSettings
+        );
+
+        // Submit buffered outputs
+        renderContext.Submit();
     }
 }
